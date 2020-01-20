84385ff5fa969ddd7006f5b1994f73b672f120fb
vishal <1013vishalsharma@gmail.com>
2020-01-20 08:29:40 +0530

    uncompleted sol: Remove duplicated ele from sorted list

---
+++ src/com/Solution/Solution82.java
@@ -0,0 +1,70 @@
+package com.Solution;
+
+public class Solution82 {
+	
+	public ListNode deleteDuplicates(ListNode head) {
+        
+		if(head == null){
+			return head;
+		}
+		else{
+			ListNode n = head;
+			ListNode prev = n;
+			ListNode i = null;
+			
+			n= n.next;
+			
+			while(n != null){
+				if(n.val != prev.val){
+					i = prev;
+					prev = n;
+					n = n.next;
+				}
+				else{
+					while(n != null && prev != null && n.val == prev.val){
+						n = n.next;
+					}
+					if(i == null){
+						head = n;
+					}
+					else{
+						i.next = n;
+						prev = i;
+					}
+				}
+			}
+		}
+		display(head);
+		return head;
+    }
+	
+	public void display(ListNode head){
+		ListNode n = head;
+		while(n != null){
+			System.out.print(n.val + " ");
+			n = n.next;
+		}
+	}
+
+	public static void main(String[] args) {
+		Solution82 sol = new Solution82();
+		ListNode head = new ListNode(1);
+		/*head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(3);
+		head.next.next.next.next = new ListNode(4);
+		head.next.next.next.next.next = new ListNode(4);
+		head.next.next.next.next.next.next = new ListNode(5);*/
+		
+		head.next = new ListNode(1);
+		head.next.next = new ListNode(1);
+		head.next.next.next = new ListNode(1);
+		//head.next.next.next = new ListNode(2);
+		//head.next.next.next.next = new ListNode(3);
+		//head.next.next.next.next.next = new ListNode(3);
+		
+		
+		sol.deleteDuplicates(head);
+	}
+
+}
