ed60166b83e5695fa8d09a44407b9273871fecce
vishal sharma <33782031+1013vishalsharma@users.noreply.github.com>
2020-01-22 13:05:02 +0530

    Remove element

--- src/com/Solution/Solution27.java
+++ src/com/Solution/Solution27.java
@@ -3,34 +3,26 @@ package com.Solution;
 public class Solution27 {
 
 	public int removeElement(int[] nums, int val) {
-		int len=0, j=nums.length-1, i=0;
-		
-		while(i <= j){
-			if(nums[i] != val){
-				i++;
-			}
-			else{
-				if(nums[i] == nums[j]){
-					j--;
-					len++;
-				}
-				else{
-					int temp = nums[i];
-					nums[i] = nums[j];
-					nums[j] = temp;
-					j--;
-					len++;
-				}
-			}
-		}
-		return nums.length-len;
+        int count = 0;
+        int j = 1;
+		for(int i=0; i< nums.length; i++) {
+        	if(nums[i] == val) {
+        		while(nums[j] == val) {
+        			j++;
+        		}
+        		nums[i] = nums[j];
+        	}
+        	else {
+        		j++;
+        	}
+        }
+		return count;
     }
 	
 	public static void main(String[] args) {
 		Solution27 sol = new Solution27();
-		int[] nums = {1};
-		//int[] nums = {0,1,2,2,3,0,4,2};
-		sol.removeElement(nums, 1);
+		int[] nums = {3,2,2,3};
+		sol.removeElement(nums, 3);
 	}
 
 }
